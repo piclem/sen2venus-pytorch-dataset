48722f7edff86f8aa7ada039b68d81e74f6525e6
Clément Peyrard <cpe@geosys.com>
2024-02-11 16:16:44 +0100

    add inference CLI, update readme

--- examples/README.md
+++ examples/README.md
@@ -1,7 +1,7 @@
 # Examples
 To run the examples, make sure you have installed the package and moved to this examples folder (`cd examples`).
 
-## `test_sen2venus.py`
+## 🏁 `test_sen2venus.py`
 The [test_sen2venus.py](./test_sen2venus.py) script showcases the `Sen2Venus` class (a torch `ConcatDataset` of `Sen2VenusSite` datasets) with a simple example of multi-site dataset  loading using 2 sites ('`SUDOUE-4`', '`FGMANAUS`').
 
 ```bash
@@ -9,7 +9,7 @@ python test_sen2venus.py
 ```
 
 
-## `test_sen2venussite.py`
+## 🏁 `test_sen2venussite.py`
 The [test_sen2venussite.py](./test_sen2venussite.py) script showcases the `Sen2VenusSite` class with a simple example of single-site dataset loading (`SUDOUE-4`').
 
 ```bash
@@ -17,8 +17,10 @@ python test_sen2venussite.py
 ```
 
 
-## `train_spl.py`
-The [train_spl.py](./train_spl.py) script showcases the full training of a SRGAN model.
+## 🤖 `train_spl.py`
+The [train_spl.py](./train_spl.py) script showcases the full training of a SRGAN model on all AOIs.
+
+⚠️ you need 100+ GB of free disk space to download all sites/subdatasets.
 
 ```bash
 # make sure you have lightning installed
@@ -27,13 +29,18 @@ pip install -U lightning
 git submodule init
 git submodule update
 # use cuda or cpu, modify the root to use a read-efficient disk location such as a local SSD disk 
-python train_spl.py --device cuda --root . 
+python train_spl.py train --device cuda --root . 
 # follow training evolution with tensorboard
 tensorboard --logdir ./lightning_logs --host 0.0.0.0
 ```
 
+If you have access to earthdaily Earth Data Store STAC catalog, you can also perform prediction on some AOI / TOI:
+```
+python train_spl.py predict <path to checkpoint> --aoi='(1.563,43.675,1.594,43.697)' toi='2023-07'
+```
 
-## `visualize_samples.py`
+
+## 🔍 `visualize_samples.py`
 The [visualize_samples.py](./visualize_samples.py) script showcases some data visualisation using matplotlib.
 ```bash
 # make sure you have matplotlib - pip install matplotlib
--- examples/train_spl.py
+++ examples/train_spl.py
@@ -5,6 +5,7 @@ import sys
 sys.path.append('sr-pytorch-lightning')
 from models import SRGAN
 from lightning import Trainer
+import torch
 from torch.utils.data import DataLoader
 
 def train(device='cpu', datasets_root='.'):
@@ -15,6 +16,53 @@ def train(device='cpu', datasets_root='.'):
     trainer = Trainer(log_every_n_steps=10)
     trainer.fit(model, train_dataloaders=dataloader)
 
+def test(checkpoint, device='cpu', datasets_root='.'):
+    raise NotImplementedError()
+
+def predict(checkpoint, device='cpu', aoi=(1.563, 43.675, 1.594, 43.697), toi='2023-07', output_path=None, save_input_tifs=True):
+    from earthdaily.earthdatastore import Auth
+    import numpy as np
+    import xarray as xr
+    import rioxarray as rxr
+    import os
+
+    tmp_file_hash = '_'.join(['-'.join([str(c) for c in aoi]), toi])
+    tmp_input_file = tmp_file_hash + '_input.zarr'
+    print("Downloading data...")
+    if not os.path.exists(tmp_input_file):
+        client = Auth()
+        data = client.datacube('sentinel-2-l2a', assets=["blue", "green", "red", "nir", "rededge1", "rededge2", "rededge3", "nir08"], datetime=toi, bbox=aoi, clear_cover=100, mask_with='scl', resampling='cubic')
+        print(f"Found {len(data.coords['time'])} clear dates: {data.coords['time'].values}")
+        data = data.to_array('band').compute()
+        data.to_zarr(tmp_input_file)
+    else:
+        data = xr.open_zarr(tmp_input_file).to_array('v').isel(v=0) # force DataArray 
+
+    # convert to torch tensor
+    data = data.transpose('time', 'band', 'y', 'x')
+    x = torch.from_numpy(np.nan_to_num(data.values, nan=0)).to('cuda') # time is batch dimension, no need to unsqueeze
+    print("Loading model...")
+    model = SRGAN.load_from_checkpoint(checkpoint)
+    model.automatic_optimization = False
+    print("Predict...")
+    with torch.no_grad():
+        y = model(x).cpu()
+    print("Save output...")
+
+    # TODO: that's probably dirty coordinates definition (corner vs center etc.) => check that
+    new_x = np.linspace(data.x[0], data.x[-1], data.sizes["x"] * 2)
+    new_y = np.linspace(data.y[0], data.y[-1], data.sizes["y"] * 2)
+    output_data = data.interp(x=new_x, y=new_y)
+    output_data.values = y
+    if output_path is None:
+        output_path = tmp_file_hash
+    os.makedirs(output_path, exist_ok=True)
+    for t in output_data.coords['time'].values:
+        output_data.sel(time=t).rio.to_raster(os.path.join(output_path, output_path+f'_{str(t)[:10]}_sr.tif'))
+        data.sel(time=t).rio.to_raster(os.path.join(output_path, output_path+f'_{str(t)[:10]}_input.tif'))
+
+    print("Done.")
+    
 if __name__ == '__main__':
-    fire.Fire(train)
+    fire.Fire({"train":train, "test":test, "predict": predict})
     
